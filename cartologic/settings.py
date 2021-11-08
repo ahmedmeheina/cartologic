b4704ece4a5b9eb3e0dfc756871389897f1ed407
ahmed meheina <ahmedmeheina@gmail.com>
2021-11-08 12:28:06 +0200

    Configuring PostGIS

--- cartologic/settings.py
+++ cartologic/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'cartologic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'cartologic',
         'USER': 'postgres',
         'PASSWORD': 'password',
