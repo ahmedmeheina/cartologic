f40e5fcba5c1b80d2769c30d1d09e243378e6021
ahmed meheina <ahmedmeheina@gmail.com>
2021-11-08 13:58:52 +0200

    Redefining location field

--- shops/models.py
+++ shops/models.py
@@ -1,9 +1,8 @@
-from django.db import models
+from  django.contrib.gis.db import models
 
 # Create your models here.
 
 class Shop(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    lon = models.FloatField()
-    lat = models.FloatField()
+    poly = models.PolygonField()
